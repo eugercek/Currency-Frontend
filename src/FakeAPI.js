b5a5c373cfba385286711682e5cfe73b518ea013
Emin Umut Gerçek <umutgercek1999@gmail.com>
2021-08-16 17:28:27 +0300

    Add fakeAPI

---
+++ src/FakeAPI.js
@@ -0,0 +1,9 @@
+const currencyNameList = ["USD", "CHF", "EUR", "CAD"];
+
+const randomBetween = (min, max) => (Math.random() * (max - min)) + min;
+
+export const currencies = currencyNameList.map((name) => ({
+    name: name,
+    buyingPrice: randomBetween(3, 5),
+    sellinPrice: randomBetween(3, 5),
+}));
