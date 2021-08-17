a5c9c70a616034ad0d7c86857b8d1693b8b10887
Emin Umut Gerçek <umutgercek1999@gmail.com>
2021-08-17 10:45:33 +0300

    Add fetch error handling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import { useEffect, useState } from 'react';
 import CurrencyTable from './CurrencyTable';
+import { Alert } from '@material-ui/lab';
 
 function App() {
-  const [currencies  , setCurrencies] = useState(null);
+  const [currencies, setCurrencies] = useState(null);
+  const [fetchError, setFetchError] = useState(false);
 
   useEffect(() => {
     fetch('http://localhost:3001/currencies')
       .then(res => res.json())
       .then(data => setCurrencies(data))
+      .catch(err => setFetchError(true));
   }, [])
 
   return (
     <div className="App">
+      {fetchError &&
+        <Alert severity="error">Coudn't fetch data</Alert>}
       {currencies && <CurrencyTable currencies={currencies} />}
     </div>
   );
