ea0c66f6fa0a841a2f0ec77195cc283c7eee5e25
Emin Umut Gerçek <umutgercek1999@gmail.com>
2021-08-17 14:56:28 +0300

    Add input fields

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import CurrencyTable from './CurrencyTable';
 import { Alert } from '@material-ui/lab';
+import { Typography, TextField, Select, MenuItem } from '@material-ui/core';
 
 function App() {
   const [currencies, setCurrencies] = useState(null);
@@ -15,10 +16,26 @@ function App() {
 
   return (
     <div className="App">
-      {fetchError && 
-      <Alert severity="error">Coudn't fetch data</Alert>}
+      <Typography variant="h2" align="center" gutterBottom>Daily Currencies</Typography>
+      {fetchError &&
+        <Alert severity="error">Coudn't fetch data</Alert>}
       {currencies && <CurrencyTable currencies={currencies} />}
-      
+      <span>&nbsp;&nbsp;</span>
+      <form noValidate autoComplete="off" align="center">
+        <Select >
+          {currencies && currencies.map((c) => <MenuItem key={c.name}>{c.name}</MenuItem>)}
+        </Select>
+
+        <TextField
+          label="Enter amount"
+          variant="outlined"
+          defaultValue="0"
+        />
+        <Select >
+          {currencies && currencies.map((c) => <MenuItem key={c.name}>{c.name}</MenuItem>)}
+        </Select>
+      </form>
+
     </div>
   );
 
