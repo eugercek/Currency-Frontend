00f2ceeb2919f1a8a6c950eab3b2cc6bfc6a7b8a
Emin Umut Gerçek <umutgercek1999@gmail.com>
2021-08-18 13:55:03 +0300

    Format code

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import { useEffect, useState } from 'react';
-import CurrencyTable from './components/CurrencyTable';
-import { Alert } from '@material-ui/lab';
-import { Typography} from '@material-ui/core';
-import Converter from './components/Converter';
+import { useEffect, useState } from "react";
+import CurrencyTable from "./components/CurrencyTable";
+import { Alert } from "@material-ui/lab";
+import { Typography } from "@material-ui/core";
+import Converter from "./components/Converter";
 
 function App() {
   const [currencies, setCurrencies] = useState(null);
   const [fetchError, setFetchError] = useState(false);
 
   useEffect(() => {
-    fetch('http://localhost:3001/currencies')
-      .then(res => res.json())
-      .then(data => setCurrencies(data))
-      .catch(err => setFetchError(true));
-  }, [])
+    fetch("http://localhost:3001/currencies")
+      .then((res) => res.json())
+      .then((data) => setCurrencies(data))
+      .catch((err) => setFetchError(true));
+  }, []);
 
   return (
     <div className="App">
-      <Typography variant="h2" align="center" gutterBottom>Daily Currencies</Typography>
-      {fetchError &&
-        <Alert severity="error">Coudn't fetch data</Alert>}
+      <Typography variant="h2" align="center" gutterBottom>
+        Daily Currencies
+      </Typography>
+      {fetchError && <Alert severity="error">Could not fetch data</Alert>}
       {currencies && <CurrencyTable currencies={currencies} />}
       <span>&nbsp;&nbsp;</span>
-      {currencies && <Converter currencyNames={currencies.map(c => c.name)}/>}
+      {currencies && (
+        <Converter currencyNames={currencies.map((c) => c.name)} />
+      )}
     </div>
   );
-
 }
-
-
 
 export default App;
--- src/components/CurrencyTable.js
+++ src/components/CurrencyTable.js
@@ -1,26 +1,34 @@
 import Currency from "./Currency";
-import { Table, TableContainer, TableHead, TableCell, TableBody } from "@material-ui/core";
+import {
+  Table,
+  TableContainer,
+  TableHead,
+  TableCell,
+  TableBody,
+} from "@material-ui/core";
 import Paper from "@material-ui/core/Paper";
 
 const CurrencyTable = ({ currencies }) => {
-    return (
-        <TableContainer component={Paper} style={{width: "60vw", margin: "auto" }}>
-            <Table >
-                <TableHead>
-                    <TableCell>ISO Name</TableCell>
-                    <TableCell>Buying Price</TableCell>
-                    <TableCell>Selling Price</TableCell>
-                </TableHead>
-                <TableBody>
-                    {currencies.map(c => <Currency
-                        name={c.name}
-                        buyingPrice={c.buyingPrice}
-                        sellingPrice={c.sellingPrice} />
-                    )}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    );
-}
+  return (
+    <TableContainer component={Paper} style={{ width: "60vw", margin: "auto" }}>
+      <Table>
+        <TableHead>
+          <TableCell>ISO Name</TableCell>
+          <TableCell>Buying Price</TableCell>
+          <TableCell>Selling Price</TableCell>
+        </TableHead>
+        <TableBody>
+          {currencies.map((c) => (
+            <Currency
+              name={c.name}
+              buyingPrice={c.buyingPrice}
+              sellingPrice={c.sellingPrice}
+            />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
 
 export default CurrencyTable;
