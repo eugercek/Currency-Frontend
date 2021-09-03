721a047d1c587bda63634833b3e97c49fa3236e6
Emin Umut Gerçek <umutgercek1999@gmail.com>
2021-09-03 14:13:51 +0300

    Add navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import CurrencyTableRoute from "./routes/CurrencyTableRoute";
 import ErrorRoute from "./routes/ErrorRoute";
 import ConvertorRoute from "./routes/ConvertorRoute";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import { Redirect } from "react-router";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
@@ -17,18 +20,41 @@ function App() {
   }, []);
 
   return (
-    <Router>
-      <Switch>
-        <Route path="/table">
-          {fetchError && <ErrorRoute />}
-          {currencies && <CurrencyTableRoute currencies={currencies} />}
-        </Route>
-        <Route path="/">
-          {fetchError && <ErrorRoute />}
-          {currencies && <ConvertorRoute currencies={currencies} />}
-        </Route>
-      </Switch>
-    </Router>
+    <div className="App">
+      <Router>
+        <AppBar position="static" color="transparent">
+          <Toolbar>
+            <Link
+              to="/table"
+              style={{ textDecoration: "none", color: "inherit" }}
+            >
+              Table
+            </Link>
+            <Link
+              to="/convertor"
+              style={{
+                textDecoration: "none",
+                marginLeft: 10,
+                color: "inherit",
+              }}
+            >
+              Convertor
+            </Link>
+          </Toolbar>
+        </AppBar>
+        <Switch>
+          <Route path="/table">
+            {fetchError && <ErrorRoute />}
+            {currencies && <CurrencyTableRoute currencies={currencies} />}
+          </Route>
+          <Route path="/convertor">
+            {fetchError && <ErrorRoute />}
+            {currencies && <ConvertorRoute currencies={currencies} />}
+          </Route>
+          <Redirect from="/" to="/convertor" />
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
